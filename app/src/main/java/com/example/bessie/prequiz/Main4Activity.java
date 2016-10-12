9a920d95248f47aa6277069ee222fdc97be35dab
Bessie <thibedibbt@gmail.com>
2016-10-12 10:39:49 +0100

    1st Commit

--- app/src/main/java/com/example/bessie/prequiz/Main4Activity.java
+++ app/src/main/java/com/example/bessie/prequiz/Main4Activity.java
@@ -1,13 +1,43 @@
 package com.example.bessie.prequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main4Activity extends AppCompatActivity {
+
+    private TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        answer= (TextView)findViewById(R.id.display);
+    }
+
+    public void incorrect(View v){
+
+        answer.setText("Oops! Wrong Answer");
+    }
+
+    public void hint (View v){
+
+        Toast.makeText(this,"It is Oval shaped.", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void correct (View v){
+
+        Intent intent = new Intent(getBaseContext(),Main3Activity.class);
+        startActivity(intent);
+    }
+    public void home (View v){
+        Intent intent = new Intent (getBaseContext(),MainActivity.class);
+        startActivity(intent);
+
     }
 }
